8bf8194f8c529cc78b0a7d16c21da44711fa4661
PaolaSanchezCastillo <paolaescastillo@gmail.com>
2021-03-07 11:18:04 -0600

    Delete completo, CRUD completo

--- Semana-13/src/routes/products.js
+++ Semana-13/src/routes/products.js
@@ -1,6 +1,9 @@
 const express = require('express'); 
+const { route } = require('.');
 const router = express.Router();
 const Product = require('./../model/Product'); 
+
+//READ
 
 router.get('/productos/listaProductos', async(req, res) =>{
     const products = await Product.find().lean();
@@ -12,7 +15,7 @@ router.get('/productos/agregarProducto',(req, res) =>{
     res.render('products/agregarProducto');
 });
 
-
+//CREATE
 router.post('/productos/agregarProducto',async(req, res) =>{
    const {_id, nombreProducto, descripcion, precio, cantidad} = req.body;
    console.log(req.body);
@@ -46,7 +49,7 @@ router.put('/productos/editarProducto/:_id', async(req, res) => {
     // encotrar el documento dentro de la collection de productos
     await Product.findByIdAndUpdate(req.params._id, {nombreProducto, descripcion, precio,cantidad});
     req.flash('successMessage', 'Producto editado correctamente'); 
-    res.redirect('products/listaProductos'); 
+    res.redirect('/productos/listaProductos'); 
 
 });
 
@@ -54,6 +57,14 @@ router.get('/productos/editarProducto/:_id' , async(req, res) =>{
     const updateProduct = await Product.findById(req.params._id).lean();
     console.log(updateProduct); 
     res.render('products/editarProducto', {updateProduct}); 
+});
+
+// DELETE 
+
+router.get('/productos/eliminarProducto/:_id', async(req, res) =>{
+    await Product.findByIdAndRemove(req.params._id); 
+    req.flash('successMessage', 'Producto eliminado correctamente'); 
+    res.redirect('/productos/listaProductos'); 
 });
 
 module.exports = router;
